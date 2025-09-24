52e6ad5fe23d6822f357753940672fafed08182c
Besu <bbesu1606@gmail.com>
2025-09-24 10:23:23 -0300

    Atualização SW

---
+++ Aula_Hamburguer/Hamburguer/sw.js
@@ -0,0 +1,43 @@
+
+const CACHE_NAME = 'my-cache-v1';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icons/icon-192.png',
+  '/icons/icon-48.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
