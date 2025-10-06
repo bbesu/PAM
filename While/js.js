44b8ab7f0f40170761dd5c981f5b31c176cd802c
Besu <bbesu1606@gmail.com>
2025-10-06 20:37:20 -0300

    Add files via upload
    revisão looping - while

---
+++ While/js.js
@@ -0,0 +1,17 @@
+let x = 0;
+const evenNumers = []
+
+while (x<10){
+    if(x%2===0)
+        {evenNumers.push(x) //adiciona elementos na lista
+            }
+    console.log('Valor atual de x:', x)
+    x++;
+}
+
+
+console.log('x agora é: ',x)
+
+console.log('');
+
+  console.log('lista de numeros pares:',evenNumers)
